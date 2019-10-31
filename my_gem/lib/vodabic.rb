0ee9e479dad6bca3d5918805bbca937b9940fe2f
MykolaPylypets <petrovychmykola@gmail.com>
2019-10-31 06:56:32 +0200

    html template added

--- my_gem/lib/vodabic.rb
+++ my_gem/lib/vodabic.rb
@@ -1,6 +1,18 @@
 class Kigh
   def self.supermethod(content, bypass = nil)
     content = (content).gsub(/<.*?>|<\/.>/, '*') unless bypass == "bypass_html: true"
-    File.open("index.html", "w") { |x| x.puts(content) } 
+    page = File.open("index.html", "w") 
+    page.puts "
+      <!DOCTYPE html>
+      <html lang=\"en\">
+      <head>
+      <title>vodabic gem</title>
+      <meta charset=\"utf-8\">
+      </head>
+      <body>
+      #{content}
+      </body>
+      </html>" 
+page.close()
   end
 end
